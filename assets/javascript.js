9d7fd58daebd0e7b22523a660b38600dc456733d
Ashton B <ashtonbrugh@gmail.com>
2021-11-16 12:21:27 -0500

    handling form submission

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,33 @@
-var response = fetch("https://api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=a9792d86ad12efe56b0f259d2b28f7cc")
-  .then(function(response){
-    response.json().then(function(data){
+var userFormEl = document.querySelector("#user-form");
+var userCity = document.querySelector("#city");
+
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+
+//get the value from the input element
+var cityInput = userCity.value.trim();
+
+if(cityInput) {
+  userInput(cityInput);
+  userCity.value = "";
+} else {
+  alert("Please enter a city!")
+}
+
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+
+
+var userInput = function(user) {
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + user + "&appid=a9792d86ad12efe56b0f259d2b28f7cc";
+
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
       console.log(data);
-    })
-  })
+    });
+  });
+};
+
